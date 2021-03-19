a80f47a6c6d479573ac0fa03e7fea24ad1dd2f76
Akpan Abraham <Akpjunio4real@gmail.com>
2021-03-19 05:34:21 +0100

    heroku mysql connection

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd2415a1c67329be3500ba038f5f9260f846d686f',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd2415a1c67329be3500ba038f5f9260f846d686f',
+    ),
+  ),
+);
